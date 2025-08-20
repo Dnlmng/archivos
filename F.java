c7cef97241972fe824811e23a2362f0ce56c95d4
Dnlmng <161865412+Dnlmng@users.noreply.github.com>
2025-08-20 17:16:14 +0000

    cambios 20/08

---
+++ F.java
@@ -0,0 +1,45 @@
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.StandardOpenOption;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class F{
+    public void read(String filename){
+        Path ruta = Paths.get(filename);
+        try (BufferedReader br = Files.newBufferedReader(ruta, StandardCharsets.UTF_8)) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void write(String filename){
+
+        try{
+        Path out = Paths.get(filename) ;
+        try (BufferedWriter bw = Files.newBufferedWriter(
+                out, StandardCharsets.UTF_8,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING)) {
+            bw.write("Encabezado\n");
+            bw.write("línea 1\n");
+        }
+        }catch(IOException e){
+        System.err.println("No se pudo obtener el archivo");
+        }
+    }
+
+
+    public static void main(String[] args){
+        F f = new F();
+        f.read("notes.txt");
+        f.write("notes.txt");
+    }
+}
